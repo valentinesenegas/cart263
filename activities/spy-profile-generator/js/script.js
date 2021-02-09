890996a6210d831bcbbce90033408eb0eddeaa9e
valentinesenegas <70824685+valentinesenegas@users.noreply.github.com>
2021-02-09 17:51:37 -0500

    Started Activity: Spy Profile Generator

---
+++ activities/spy-profile-generator/js/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+Spy Profile Generator
+Valentine Sénégas
+*/
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
