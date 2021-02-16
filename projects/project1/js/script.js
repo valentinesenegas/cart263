29fe7e3b11b249e05c8e56b7ca050e6b5dc2838c
valentinesenegas <70824685+valentinesenegas@users.noreply.github.com>
2021-02-16 09:43:26 -0500

    P1: Starting code

---
+++ projects/project1/js/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+The Platform
+Valentine Sénégas
+
+This is a game inspired by the movie The Platform directed by Galder Gaztelu-Urrutia.
+*/
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
