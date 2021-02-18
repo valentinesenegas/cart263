012e7e306d99b2a7454b8b629059c026cb8c933e
valentinesenegas <70824685+valentinesenegas@users.noreply.github.com>
2021-02-18 09:08:37 -0500

    Started Activity Bubble Popper

---
+++ activities/bubble-popper/js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/**
+Bubble Popper
+Valentine Sénégas
+
+*/
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
