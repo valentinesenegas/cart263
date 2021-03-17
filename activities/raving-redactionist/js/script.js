90d37d41de8f60e298928055e8a9d5b5a6b1b54b
valentinesenegas <70824685+valentinesenegas@users.noreply.github.com>
2021-03-17 18:26:00 -0400

    Started Raving Redactionist activity

---
+++ activities/raving-redactionist/js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/**
+Raving Redactionist
+Valentine Sénégas
+
+*/
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
