1b99d7e21af971c3e18007dd136fd0567ea9c7b1
valentinesenegas <70824685+valentinesenegas@users.noreply.github.com>
2021-03-24 23:30:29 -0400

    Started Code Taker activity

---
+++ activities/code-taker/js/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/**
+Code Taker
+Valentine Sénégas
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
