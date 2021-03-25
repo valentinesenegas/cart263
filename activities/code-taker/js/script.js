f96630f25ebe9db3b242c1571f86fa3122c5feef
valentinesenegas <70824685+valentinesenegas@users.noreply.github.com>
2021-03-24 23:59:11 -0400

    Completed Code Taker activity

--- activities/code-taker/js/script.js
+++ activities/code-taker/js/script.js
@@ -4,29 +4,34 @@
 Code Taker
 Valentine Sénégas
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Da Vinci?!
 */
 
-/**
-Description of preload
-*/
-function preload() {
+$(`#solved-dialog`).dialog({
+  autoOpen: false,
+  buttons: {
+    "I know.": function() {
+      $(this).dialog(`close`);
+    }
+  }
+});
 
-}
+$(`.secret`).on(`mouseover`, function(event) {
+  $(this).addClass(`found`, 500);
+  $(this).draggable({
+    helper: `clone`
+  });
+});
 
-
-/**
-Description of setup
-*/
-function setup() {
-
-}
-
-
-/**
-Description of draw()
-*/
-function draw() {
-
-}
+$(`#answer`).droppable({
+  drop: function(event, ui) {
+    let letter = ui.draggable.text();
+    $(this).append(letter);
+    ui.draggable.draggable(`disable`);
+    ui.draggable.removeClass(`found`);
+    // Check if they got it
+    if ($(this).text() === `Theremin`) {
+      $(`#solved-dialog`).dialog(`open`);
+    }
+  }
+});
